cb934739898d616b1fe027aedc19a499fa8b89dc
murimolda <murimolda@gmail.com>
2022-02-03 18:56:47 +0300

    fix: change function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
   let bracketsOpenArray = [];
   let bracketsPairObject = {};
+  let bracketsIdenticalPair = {};
   let stack = [];
   let string = str;
 
   for (let k = 0; k < bracketsConfig.length; k++) {
     bracketsOpenArray.push(bracketsConfig[k][0])
     bracketsPairObject[`${bracketsConfig[k][1]}`] = `${bracketsConfig[k][0]}`;
+    if (bracketsConfig[k][1] === bracketsConfig[k][0]) {
+      bracketsIdenticalPair[`${bracketsConfig[k][1]}`] = `${bracketsConfig[k][0]}`;
+    }
   }
-
   for (let i = 0; i < string.length; i++) {
     if (string[i + 1] in bracketsPairObject && bracketsPairObject[`${string[i + 1]}`] === string[i]) {
       let a = `${string[i]}${string[i + 1]}`;
       string = string.replace(a, '');
+      i--;
     }
   }
+
   if (string === '') {
     return true;
   }
@@ -22,24 +27,29 @@ module.exports = function check(str, bracketsConfig) {
   for (let i = 0; i < string.length; i++) {
     let openSymbol = string[i];
     let stackSymbol = stack[stack.length - 1];
-    if (stackSymbol === openSymbol) {
-      stack.pop();
-      if (stack.length === 0) {
-        return true;
-      }
-    }
     if (bracketsOpenArray.includes(openSymbol)) {
-      stack.push(openSymbol);
+      if (openSymbol !== bracketsIdenticalPair[stackSymbol]) {
+        stack.push(openSymbol);
+      } else {
+        stack.pop();
+      }
     } else {
       if (stack.length === 0) {
         return false;
       }
       if (bracketsPairObject[openSymbol] === stackSymbol) {
         stack.pop();
+        console.log(stack)
       } else {
         return false;
       }
     }
+
   }
-  return stack.length === 0;
+  if (stack.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
+
